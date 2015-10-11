1b77a54d583eeedf1c22ee3374b5f14ed97059d9
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-10-11 15:34:51 -0400

    basic stack module created

---
+++ stack.js
@@ -0,0 +1,37 @@
+'use strict'
+
+function Stack() {
+    this.stack = [];
+    this.index = -1;
+}
+
+Stack.prototype.push = function(value) {
+    value = value || null;
+    this.index++;
+    this.stack[this.index] = value || null;
+    return value;
+};
+
+Stack.prototype.pop = function() {
+    if (this.index === -1) return undefined;
+    let value = this.stack[this.index];
+    this.stack.splice(this.index);
+    this.index--;
+    return value;
+}
+
+Stack.prototype.peek = function() {
+    return this.stack[this.index];
+}
+
+Stack.prototype.isEmpty = function() {
+    return this.index === -1;
+}
+
+Stack.prototype.empty = function() {
+    this.stack = [];
+    this.index = -1;
+    return true;
+}
+
+module.exports = Stack;
