b07314fbf0411f88d318d48878b81019dbc98708
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-10-11 20:11:06 -0400

    framework for future methods, quickbuild helper, and can pass arguments to make list upon object creation

--- linkedlist.js
+++ linkedlist.js
@@ -5,8 +5,14 @@ function Node(data) {
     this.next = null;
 }
 
+//empty list with new List() or pass it arguments to build a list immediately
 function List() {
     this.head = null;
+    if (arguments) {
+        for (var i = 0, len = arguments.length; i < len; i++) {
+            this.append(arguments[i]);
+        }
+    }
 }
 
 List.prototype.prepend = function(data) {
@@ -20,12 +26,44 @@ List.prototype.append = function(data) {
         this.head = new Node(data);
         return;
     }
-    
+
     let node = this.head;
     while (node.next) {
         node = node.next;
     }
     node.next = new Node(data);
+}
+
+List.prototype.insert = function(index, data) {
+
+}
+
+List.prototype.getNth = function(index) {
+
+}
+
+List.prototype.indexOf = function(data) {
+
+}
+
+List.prototype.lastIndexOf = function(data) {
+
+}
+
+List.prototype.removeByIndex = function(index) {
+
+}
+
+List.prototype.removeFirstByValue = function(data) {
+
+}
+
+List.prototype.removeLastByValue = function(data) {
+
+}
+
+List.prototype.removeAllByValue = function(data) {
+
 }
 
 
@@ -40,9 +78,13 @@ List.prototype.toString = function() {
     return string;
 }
 
+//just a little helper to build a randomized list to work with
+List.prototype.quickBuild = function(length) {
+    for (let i = 0; i < length; i++) {
+        this.append(Math.floor(Math.random() * 100));
+    }
+}
+
+
+
 module.exports = List;
-
-
-
-
-//todo: append, insert, findNth, remove...any others?
