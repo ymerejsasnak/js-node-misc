6d931086787549961c08200281655bdbe9d53e31
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-10-14 21:39:23 -0400

    started bad sentence generator

--- sentencegenerator.js
+++ sentencegenerator.js
@@ -1,1 +1,18 @@
 //word lists of nouns, verbs, adjectives, adverbs, prepositions, etc.
+
+'use strict'
+
+function makeWordList() {
+    wordList = {nouns: ['chair', 'bed', 'man', 'tree', 'finger', 'hamburger'],
+                verbs: ['ran', 'sneezed', 'tickled', 'thought', 'cried'],
+                adjectives: ['fat', 'wistful', 'grizzled', 'cranky'],
+                adverbs: ['quickly', 'slickly', 'shyly']
+            };
+    return wordList;
+}
+
+
+//return random-ish item from an array
+function sample(array) {
+
+}
