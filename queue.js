f91a9d44edfdfaac9428014da031335fd7a2e6fd
Jeremy A Kansas <ymerejsasnak@gmail.com>
2015-10-20 16:57:34 -0400

    simple limited naive priority queue

--- queue.js
+++ queue.js
@@ -59,10 +59,38 @@ Deque.prototype.back = function() {
 
 
 
+//will use 1 - 10 as priority value 
+// usually uses a heap, but I don't know how to do that yet, so I had an idea to do it this way (super naive and inefficient I guess)
+function PriorityQueue() {
+    this.queue = [[],[],[],[],[],[],[],[],[],[]];
+}
+
+PriorityQueue.prototype.enqueue = function(value, priority) {
+    if (priority < 1 || priority > 10) return new Error ('priority out of range (1-10)');
+    this.queue[10 - priority].push(value);
+}
+
+PriorityQueue.prototype.dequeue = function() {
+    let value;    
+    this.queue.forEach(function(array) {
+        if (array.length !== 0 && value === undefined) {
+            value = array.shift();
+        }
+    });
+    return value;
+}
+
+PriorityQueue.prototype.toString = function() {
+    return this.queue.reduce(function(a, b) {
+        return a.concat(b);
+    }).toString();
+}
 
 
 
 
 
 
-module.exports = {Queue, Deque};
+
+
+module.exports = {Queue, Deque, PriorityQueue};
